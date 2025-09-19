828b5ad46ad7e7d421488afe52122ef9824745f4
Aishwarya S <88452930+AishwaryaS9@users.noreply.github.com>
2025-09-19 11:23:07 +0530

    chore: Updated DashboardLayout file

---
+++ src/components/layout/DashboardLayout.tsx
@@ -0,0 +1,34 @@
+import type { LayoutProps } from '../../utils/interface';
+import Navbar from './Navbar';
+import SideMenu from './SideMenu';
+
+const DashboardLayout = ({ children, activeMenu }: LayoutProps) => {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <header role="banner" aria-label="Dashboard navigation bar">
+                <Navbar activeMenu={activeMenu} />
+            </header>
+
+            <div className="flex flex-1 pt-[60px]">
+                <aside
+                    className="max-[1080px]:hidden"
+                    role="navigation"
+                    aria-label="Sidebar menu"
+                >
+                    <SideMenu activeMenu={activeMenu} />
+                </aside>
+
+                <main
+                    role="main"
+                    aria-label="Dashboard main content"
+                    className="grow mx-5 focus:outline-none"
+                    tabIndex={-1}
+                >
+                    {children}
+                </main>
+            </div>
+        </div>
+    );
+};
+
+export default DashboardLayout;
